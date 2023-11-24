ada46ef1076da75cf7b90b13467d0cca1036ba0d
Pratul <ptpratul2@gmail.com>
2023-11-24 17:22:04 +0530

    Final changes In contact

--- octavision/www/contact/contact.py
+++ octavision/www/contact/contact.py
@@ -2,18 +2,14 @@ from __future__ import unicode_literals
 import frappe
 from frappe import _
 
+
 @frappe.whitelist(allow_guest=True)
-def create_contact_us(contact_name, phone, message, email):
-    try:
-        print("Creating contact...")
-        contact = frappe.new_doc("Website Contact")
-        contact.contact_name = contact_name
-        contact.email = email
-        contact.phone = phone
-        contact.message = message
-        contact.save()
-        print("Contact saved successfully!")
-        frappe.msgprint("Successfully Sent! We will contact you soon.", "Success")
-    except Exception as e:
-        print("Error in create_contact_us:", str(e))
-        frappe.log_error("Error in create_contact_us: {}".format(str(e)))
+def create_contact_us(contact_name, phone, query_type, message, email):
+	contact = frappe.new_doc("Website Contact")
+	contact.contact_name = contact_name	
+	contact.email = email
+	contact.phone = phone
+	contact.message = message
+	contact.query_type = query_type
+	contact.save()
+	
