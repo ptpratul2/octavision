d7de5b38fe5cb5293a1b2ad8ade50d4201d98167
Pratul <ptpratul2@gmail.com>
2023-11-24 13:44:58 +0530

    contact page change

--- octavision/www/contact/contact.py
+++ octavision/www/contact/contact.py
@@ -3,14 +3,17 @@ import frappe
 from frappe import _
 
 @frappe.whitelist(allow_guest=True)
-def create_contact_us(contact_name, company_name, phone, business_domain, no_of_employees, message, email):
-	contact = frappe.new_doc("Website Contact")
-	contact.contact_name = contact_name
-	contact.company_name = company_name
-	contact.email = email
-	contact.phone = phone
-	contact.message = message
-	contact.business_domain = business_domain
-	contact.no_of_employees = no_of_employees
-	contact.save()
-	
+def create_contact_us(contact_name, phone, message, email):
+    try:
+        print("Creating contact...")
+        contact = frappe.new_doc("Website Contact")
+        contact.contact_name = contact_name
+        contact.email = email
+        contact.phone = phone
+        contact.message = message
+        contact.save()
+        print("Contact saved successfully!")
+        frappe.msgprint("Successfully Sent! We will contact you soon.", "Success")
+    except Exception as e:
+        print("Error in create_contact_us:", str(e))
+        frappe.log_error("Error in create_contact_us: {}".format(str(e)))
